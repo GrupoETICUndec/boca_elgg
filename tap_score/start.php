2e57f7a44155a365dcd547dda1eb9226e3999645
hditx <pablogabrieloporto@gmail.com>
2017-08-01 13:25:43 -0300

    Agregando plugins y carpeta run

--- tap_score/start.php
+++ tap_score/start.php
@@ -3,6 +3,17 @@
     elgg_register_event_handler('init', 'system', 'tap_score_init');
 
 
+//Widget functions
+        function hello_init() {
+            elgg_register_widget_type('helloworld', 'Hello, world!', 'The "Hello, world!" widget');
+        }
+
+        elgg_register_event_handler('init', 'system', 'hello_init');
+
+
+
+
+//Plugin functions
     function tap_score_init() 
     {
             $funcion = 'tap_score_page_handler';
